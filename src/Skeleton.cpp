34ce0b9ddd1a5b70ad0221de7f8485343dc52c00
csb30 <30balint@gmail.com>
2023-11-07 02:09:29 +0100

    spehres (todo: scale, color, doppler)

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : 
-// Neptun : 
+// Nev    : Cseh Balint Istvan
+// Neptun : WRNJPE
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
@@ -33,83 +33,252 @@
 //=============================================================================================
 #include "framework.h"
 
-// vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
-const char * const vertexSource = R"(
-	#version 330				// Shader 3.3
-	precision highp float;		// normal floats, makes no difference on desktop computers
+//raytrace.cpp
+struct Material {
+    vec3 ka, kd, ks; //ambiens, diffuz, spekularis
+    float  shininess;
+    Material(vec3 _kd, vec3 _ks, float _shininess) : ka(_kd * M_PI), kd(_kd), ks(_ks) { shininess = _shininess; }
+};
 
-	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+struct Hit {
+    float t;
+    vec3 position, normal; //metszes pozija, normalvektora
+    Material * material;
+    Hit() { t = -1; }
+};
+
+struct Ray {
+    vec3 start, dir; //kezdopont, iranyvektor
+    Ray(vec3 _start, vec3 _dir) { start = _start; dir = normalize(_dir); }
+};
+
+class Intersectable {
+protected:
+    Material * material;
+public:
+    virtual Hit intersect(const Ray& ray) = 0;
+};
+
+struct Sphere : public Intersectable {
+    vec3 center;
+    float radius;
+
+    Sphere(const vec3& _center, float _radius, Material* _material) {
+        center = _center;
+        radius = _radius;
+        material = _material;
+    }
+
+    Hit intersect(const Ray& ray) {
+        Hit hit;
+        vec3 dist = ray.start - center;
+        float a = dot(ray.dir, ray.dir);
+        float b = dot(dist, ray.dir) * 2.0f;
+        float c = dot(dist, dist) - radius * radius;
+        float discr = b * b - 4.0f * a * c;
+        if (discr < 0) return hit;
+        float sqrt_discr = sqrtf(discr);
+        float t1 = (-b + sqrt_discr) / 2.0f / a;	// t1 >= t2 for sure
+        float t2 = (-b - sqrt_discr) / 2.0f / a;
+        if (t1 <= 0) return hit;
+        hit.t = (t2 > 0) ? t2 : t1;
+        hit.position = ray.start + ray.dir * hit.t;
+        hit.normal = (hit.position - center) * (1.0f / radius);
+        hit.material = material;
+        return hit;
+    }
+};
+
+class Camera {
+    vec3 eye, lookat, right, up;
+public:
+    void set(vec3 _eye, vec3 _lookat, vec3 vup, float fov) { //kamera pont, hova nez, merre van a felfele, radianban nyilasszog
+        eye = _eye;
+        lookat = _lookat;
+        vec3 w = eye - lookat;
+        float focus = length(w);
+        right = normalize(cross(vup, w)) * focus * tanf(fov / 2);
+        up = normalize(cross(w, right)) * focus * tanf(fov / 2);
+    }
+    Ray getRay(int X, int Y) {
+        vec3 dir = lookat + right * (2.0f * (X + 0.5f) / windowWidth - 1) + up * (2.0f * (Y + 0.5f) / windowHeight - 1) - eye;
+        return Ray(eye, dir);
+    }
+};
+
+struct Light {
+    vec3 direction;
+    vec3 Le;
+    Light(vec3 _direction, vec3 _Le) {
+        direction = normalize(_direction);
+        Le = _Le;
+    }
+};
+
+float rnd() { return (float)rand() / RAND_MAX; }
+
+const float epsilon = 0.0001f; //kerekites/korrekcio
+
+class Scene {
+    std::vector<Intersectable *> objects;
+    std::vector<Light *> lights;
+    Camera camera;
+    vec3 La; //ambiens feny
+public:
+    void build() {
+        vec3 eye = vec3(0, 0, 2), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
+        float fov = 45 * M_PI / 180;
+        camera.set(eye, lookat, vup, fov);
+
+        La = vec3(1.0f, 1.0f, 1.0f);
+        //vec3 lightDirection(1, 1, 1), Le(2, 2, 2);
+        //lights.push_back(new Light(lightDirection, Le));
+
+        vec3 kd(0.3f, 0.2f, 0.1f), ks(2, 2, 2);
+        Material * material = new Material(kd, ks, 50);
+        for (int i = 0; i < 100; i++)
+            objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), 0.07f, material));
+    }
+
+    void render(std::vector<vec4>& image) {
+        for (int Y = 0; Y < windowHeight; Y++) {
+#pragma omp parallel for
+            for (int X = 0; X < windowWidth; X++) {
+                vec3 color = trace(camera.getRay(X, Y));
+                image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
+            }
+        }
+    }
+
+    Hit firstIntersect(Ray ray) {
+        Hit bestHit;
+        for (Intersectable * object : objects) {
+            Hit hit = object->intersect(ray); //  hit.t < 0 if no intersection
+            if (hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t))  bestHit = hit;
+        }
+        if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
+        return bestHit;
+    }
+
+    bool shadowIntersect(Ray ray) {	// for directional lights
+        for (Intersectable * object : objects) if (object->intersect(ray).t > 0) return true;
+        return false;
+    }
+
+    vec3 trace(Ray ray, int depth = 0) {
+        Hit hit = firstIntersect(ray);
+        if (hit.t < 0) return vec3(0,0,0);
+        vec3 outRadiance = hit.material->ka * La * (3-hit.t);  //numerikus integralos magic
+        for (Light * light : lights) {
+            Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
+            float cosTheta = dot(hit.normal, light->direction);
+            if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
+                outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
+                vec3 halfway = normalize(-ray.dir + light->direction);
+                float cosDelta = dot(hit.normal, halfway);
+                if (cosDelta > 0) outRadiance = outRadiance + light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
+            }
+        }
+        return outRadiance;
+    }
+};
+
+GPUProgram gpuProgram; // vertex and fragment shaders
+Scene scene;
+
+// vertex shader in GLSL
+const char *vertexSource = R"(
+	#version 330
+    precision highp float;
+
+	layout(location = 0) in vec2 cVertexPosition;	// Attrib Array 0
+	out vec2 texcoord;
 
 	void main() {
-		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
+		texcoord = (cVertexPosition + vec2(1, 1))/2;							// -1,1 to 0,1
+		gl_Position = vec4(cVertexPosition.x, cVertexPosition.y, 0, 1); 		// transform to clipping space
 	}
 )";
 
 // fragment shader in GLSL
-const char * const fragmentSource = R"(
-	#version 330			// Shader 3.3
-	precision highp float;	// normal floats, makes no difference on desktop computers
-	
-	uniform vec3 color;		// uniform variable, the color of the primitive
-	out vec4 outColor;		// computed color of the current pixel
+const char *fragmentSource = R"(
+	#version 330
+    precision highp float;
+
+	uniform sampler2D textureUnit;
+	in  vec2 texcoord;			// interpolated texture coordinates
+	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
 
 	void main() {
-		outColor = vec4(color, 1);	// computed color is the color of the primitive
+		fragmentColor = texture(textureUnit, texcoord);
 	}
 )";
 
-GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+class FullScreenTexturedQuad {
+    unsigned int vao;	// vertex array object id and texture id
+    int windowWidth, windowHeight;
+    std::vector<vec4> image;
+    Texture texture;
+
+public:
+    FullScreenTexturedQuad(int windowWidth, int windowHeight):
+    windowWidth(windowWidth), windowHeight(windowWidth), image(std::vector<vec4>(windowWidth*windowHeight)),
+    texture(windowWidth, windowHeight, image)
+    {
+        glGenVertexArrays(1, &vao);	// create 1 vertex array object
+        glBindVertexArray(vao);		// make it active
+
+        unsigned int vbo;		// vertex buffer objects
+        glGenBuffers(1, &vbo);	// Generate 1 vertex buffer objects
+
+        // vertex coordinates: vbo0 -> Attrib Array 0 -> vertexPosition of the vertex shader
+        glBindBuffer(GL_ARRAY_BUFFER, vbo); // make it active, it is an array
+        float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };	// two triangles forming a quad
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);     // stride and offset: it is tightly packed
+    }
+
+    void renderScreen(){
+        scene.render(image);
+        texture.create(windowWidth, windowHeight, image);
+    }
+
+    void Draw() {
+        glBindVertexArray(vao);	// make the vao and its vbos active playing the role of the data source
+        gpuProgram.setUniform(texture, "textureUnit");
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);	// draw two triangles forming a quad
+    }
+};
+
+FullScreenTexturedQuad * fullScreenTexturedQuad;
+
 
 // Initialization, create an OpenGL context
 void onInitialization() {
-	glViewport(0, 0, windowWidth, windowHeight);
+    glViewport(0, 0, windowWidth, windowHeight);
+    scene.build();
 
-	glGenVertexArrays(1, &vao);	// get 1 vao id
-	glBindVertexArray(vao);		// make it active
+    fullScreenTexturedQuad = new FullScreenTexturedQuad(windowWidth, windowHeight);
 
-	unsigned int vbo;		// vertex buffer object
-	glGenBuffers(1, &vbo);	// Generate 1 buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-	float vertices[] = { -0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f };
-	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-		sizeof(vertices),  // # bytes
-		vertices,	      	// address
-		GL_STATIC_DRAW);	// we do not change later
+    long timeStart = glutGet(GLUT_ELAPSED_TIME);
+    fullScreenTexturedQuad->renderScreen();
+    long timeEnd = glutGet(GLUT_ELAPSED_TIME);
+    printf("Rendering time: %d milliseconds\n", (timeEnd - timeStart));
 
-	glEnableVertexAttribArray(0);  // AttribArray 0
-	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-		0, NULL); 		     // stride, offset: tightly packed
-
-	// create program for the GPU
-	gpuProgram.create(vertexSource, fragmentSource, "outColor");
+    // create program for the GPU
+    gpuProgram.create(vertexSource, fragmentSource, "fragmentColor");
 }
 
 // Window has become invalid: Redraw
 void onDisplay() {
-	glClearColor(0, 0, 0, 0);     // background color
-	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
-
-	// Set color to (0, 1, 0) = green
-	int location = glGetUniformLocation(gpuProgram.getId(), "color");
-	glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
-
-	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
-							  0, 1, 0, 0,    // row-major!
-							  0, 0, 1, 0,
-							  0, 0, 0, 1 };
-
-	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
-	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
-
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
-
-	glutSwapBuffers(); // exchange buffers for double buffering
+    fullScreenTexturedQuad->Draw();
+    glutSwapBuffers();									// exchange the two buffers
 }
+
+
+//skeleton.cpp
+
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
